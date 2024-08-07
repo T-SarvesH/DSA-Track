5ed2a4aec63099c2dcf82887179f55e51225131c
T-SarvesH <sarvesh1.tikekar@gmail.com>
2024-08-07 20:50:45 +0530

    New daily qns

---
+++ Prac/temp.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string ch = "01";
+    int lm = stoi(ch);
+
+    cout<< lm << endl;
+    return 0;
+}
