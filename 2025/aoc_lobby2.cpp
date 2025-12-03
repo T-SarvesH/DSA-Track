0c2290444910ea570a08116585c7d81ff7c0f7d6
SarveshTikekar <sarvesh1.tikekar@gmail.com>
2025-12-03 20:08:59 +0530

    Day 3 done and dusted

--- 2025/aoc_lobby2.cpp
+++ 2025/aoc_lobby2.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
-
 using namespace std;
+
+//Construct lex largest string of len 12 from available chars
+string lexGreater(string &inp){
+	
+	string ans = "";
+	vector<char> s;
+	
+	int toDrop = inp.size() - 12;
+
+	for(char &i: inp){
+		
+		while(!s.empty() && s.back() < i && toDrop > 0){
+			
+			s.pop_back(); --toDrop;
+		}
+
+		s.push_back(i);
+	}
+
+	while(s.size() > 12)
+		s.pop_back();
+
+	return string(s.begin(), s.begin() + 12);
+}
 
 void helper(vector<string> &input){
 	
@@ -8,29 +31,11 @@ void helper(vector<string> &input){
 	long long ans = 0;
 
 	for(string &inp: input){
-
 		
-		int leftHighestBit = -1, rightHighestBit = -1, idx;
-
-		for(int i=0; i < inp.size() - 1; ++i){
-			
-			if(leftHighestBit < inp[i] - '0'){
-
-				leftHighestBit = inp[i] - '0';
-				idx = i;
-			}
-		}
-
-		for(int i=inp.size()-1; i>0; --i){
-
-			if(idx == i)
-			break;
-
-			rightHighestBit = max(rightHighestBit, inp[i] - '0');
-		}
-		
-		ans += (stoll(to_string(leftHighestBit) + to_string(rightHighestBit)));
+		string lexLargest = lexGreater(inp);	
+		ans += stoll(lexLargest);
 	}
+
 	cout << "The answer is: " << ans << endl;
 }
 
