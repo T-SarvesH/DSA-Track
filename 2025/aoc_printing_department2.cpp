c6342578b31f2e70c46ea43035ce4bb1ea98c804
T-SarvesH <sarvesh1.tikekar@gmail.com>
2025-12-04 16:17:56 +0530

    Day 4 done and dusted

---
+++ 2025/aoc_printing_department2.cpp
@@ -0,0 +1,81 @@
+/* Day 4: Part 2 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> directions = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}, {-1, -1}, {1, -1}, {1, 1}, {-1, 1}};
+
+bool check(vector<string> &input, int &x, int &y, int &m, int &n){
+
+	int count = 0;
+	
+	for(auto dir: directions){
+		
+		int newX = x + dir[0];
+		int newY = y + dir[1];
+		
+
+		if(newX >=0 && newX < m && newY >=0 && newY < n && input[newX][newY] == '@')
+			++count;
+	}
+
+	return count < 4;
+	
+}
+
+void helper(vector<string> &input){
+	
+	
+	long long ans = 0;
+	int m = input.size(), n = input[0].size();
+	
+	vector<vector<int>> setCoords;
+
+	while(true){
+		
+		int initCount = 0;
+		for(int i=0; i<m; ++i){
+
+			for(int j=0; j<n; ++j){
+			
+				if(input[i][j] == '@' && check(input, i, j, m, n)){
+					
+					setCoords.push_back({i, j});
+					++initCount;
+				}
+			}
+		}
+
+		if(!initCount)
+			break;
+
+		ans += initCount;
+
+		for(auto &sc: setCoords)
+			input[sc[0]][sc[1]] = '.';
+
+		setCoords.clear();
+	}
+
+	cout << "The answer is: " << ans << endl;
+}
+
+int main(){
+
+	vector<string> input;
+	string temp;
+
+	while(true){
+
+		cin >> temp;
+
+		if(temp == "t")
+			break;
+	
+		input.push_back(temp);
+	}
+
+	helper(input);
+	
+	return 0;
+}
